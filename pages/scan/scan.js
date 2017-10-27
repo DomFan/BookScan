7bf1d098ad3e42796e3df9f6fd14d6dd8a09ced9
MaxwellHuang2015 <410116635@qq.com>
2017-10-27 12:14:47 +0800

    书名、作者、译者等信息扫码录入功能实现，还差标签和评价

--- pages/scan/scan.js
+++ pages/scan/scan.js
@@ -1,15 +1,37 @@
-// example/user/book/book.js
-var articleTitle = "Title";
-var articleAuthor = "Author";
+var Bmob = require('../../utils/bmob.js');
+var common = require('../../utils/common.js');
+
+var title = "Title";
+var subtitle = "Subtitle";
+var author = ["Author"];
+var translator = ["Translator"];
 var isbn = "XXXXXX";
+var binding = "无";
+var series = "无";
+var summary = "Summary";
+var author_intro = "Author Intro";
+var publisher = "Publisher";
+var pages = "1";
 var imageSource = "/image/default_book_pic.jpg";
+var tags;
+var rating;
 
 Page({
   data: {
-    articleTitle: articleTitle,
-    articleAuthor: articleAuthor,
+    articleTitle: title,
+    articleAuthor: author,
     isbn: isbn,
-    imageSource: imageSource
+    imageSource: imageSource,
+    subtitle: subtitle,
+    translator: translator,
+    binding: binding,
+    series: series,
+    summary: summary,
+    author_intro: author_intro,
+    publisher: publisher,
+    pages: pages,
+    tags: tags,
+    rating: rating
   },
 
   onLoad: function (options) {
@@ -21,11 +43,12 @@ Page({
     wx.scanCode({
       onlyFromCamera: false,
       success: function (res) {
-        console.log(res.result);
+
         isbn = res.result;
         that.setData({
           isbn: isbn,
         });
+
         wx.request({
           url: 'https://api.douban.com/v2/book/isbn/' + isbn,
           data: {},
@@ -33,19 +56,157 @@ Page({
           header: {
             "Content-Type": "json"
           },//Keep this in Development reference
+
           success: function (res) {
-            articleTitle = res.data.title;
-            articleAuthor = res.data.author;
+
             console.log(res);
+            if (res.data.hasOwnProperty("title") && res.data.title) {
+              title = res.data.title;
+            }
+            else {
+              title = "无标题";
+            }
+
+            if (res.data.hasOwnProperty("subtitle") && res.data.subtitle) {
+              subtitle = res.data.subtitle;
+            }
+            else {
+              subtitle = "无副标题";
+            }
+
+            if (res.data.hasOwnProperty("author") && res.data.author.length) {
+              author = res.data.author;
+            }
+            else {
+              author = ["无作者"];
+            }
+
+            if (res.data.hasOwnProperty("translator") && res.data.translator.length) {
+              translator = res.data.translator;
+            }
+            else {
+              translator = ["无译者"];
+            }
+
+            if (res.data.hasOwnProperty("series") && res.data.series) {
+              series = res.data.series;
+            }
+            else {
+              series = "无系列信息";
+            }
+
+            if (res.data.hasOwnProperty("binding") && res.data.binding) {
+              binding = res.data.binding;
+            }
+            else {
+              binding = "无装裱信息";
+            }
+
+            if (res.data.hasOwnProperty("summary") && res.data.summary) {
+              summary = res.data.summary;
+            }
+            else {
+              summary = "无概要";
+            }
+
+            if (res.data.hasOwnProperty("author_intro") && res.data.author_intro) {
+              author_intro = res.data.author_intro;
+            }
+            else {
+              author_intro = "无作者简介";
+            }
+
+            if (res.data.hasOwnProperty("pages") && res.data.pages) {
+              pages = res.data.pages;
+            }
+            else {
+              pages = 0;
+            }
+
+            if (res.data.hasOwnProperty("publisher") && res.data.publisher) {
+              publisher = res.data.publisher;
+            }
+            else {
+              publisher = "无出版社信息";
+            }
+            
             if (res.data.images && res.data.images.large) {
               imageSource = res.data.images.large;
             }
+
+            //TODO Tags and ratings
+
+
             that.setData({
-              articleTitle: articleTitle,
-              articleAuthor: articleAuthor,
+              articleTitle: title,
+              articleAuthor: author,
               isbn: isbn,
-              imageSource: imageSource
+              imageSource: imageSource,
+              subtitle: subtitle,
+              translator: translator,
+              binding: binding,
+              series: series,
+              summary: summary,
+              author_intro: author_intro,
+              publisher: publisher,
+              pages: pages,
+              tags: tags,
+              rating: rating
             });
+
+            wx.showModal({
+              title: '图书信息获取成功',
+              content: '请点击确认键将图书信息上传到数据库中',
+              success: function(res) {
+                if (res.confirm) {
+                  console.log('用户点击确定');
+                  var Book = Bmob.Object.extend("book");
+                  var query = new Bmob.Query(Book);
+                  query.equalTo("isbn",isbn);
+                  query.equalTo("title",title);
+                  query.find({
+                    success: function(Result) {
+                      console.log(Result);
+                      if (Result.length) {
+                        var booknumber = Result.get("number");
+                        Result.set("number",booknumber++);
+                      }
+                      else {
+                        var book = new Book();
+                        console.log("ddddd");
+                        book.set("number",1);     //数量置1
+                        book.set("isbn", isbn);   //isbn号
+                        book.set("title", title);
+                        book.set("subtitle", subtitle);
+                        book.set("subtitle", subtitle);
+                        book.set("author", author);
+                        book.set("translator", translator);
+                        book.set("binding", binding);
+                        book.set("series", series);
+                        book.set("summary", summary);
+                        book.set("author_intro", author_intro);
+                        book.set("publisher", publisher);
+                        book.set("pages", pages);
+                        book.set("image", imageSource);
+                        // book.set("tags", tags);
+                        // book.set("rating", rating);
+
+                        book.save(null, {
+                          success: function (r) {
+                            console.log("666666");
+                          },
+                          error: function (r,error) {
+                            console.log(error);
+                          }
+                        });
+                      }
+                    }
+                  });
+                } else if (res.cancel) {
+                  console.log('用户点击取消');
+                }
+              }
+            })
           }
         })
       }
